721b69a5c5e1ddc103ef387fde77ce8e818bd3b4
baj12 <bernd.jagla@pasteur.fr>
2019-07-16 11:04:24 +0200

    git problem

--- traj_trajectories/reactives.R
+++ traj_trajectories/reactives.R
@@ -705,15 +705,7 @@ traj_elpi_modules <- reactive({
   gene_sel <- traj_elpi_gimp()
   seed <- input$elpiSeed
 
-<<<<<<< HEAD
   if (is.null(scEx_log) | is.null(gene_sel) | elpimode=="computeElasticPrincipalCircle" | nrow(gene_sel$gene_sel)<1) {
-=======
-<<<<<<< HEAD
-  if (is.null(scEx_log) | is.null(gene_sel) | elpimode=="computeElasticPrincipalCircle" | nrow(gene_sel$gene_sel)<1) {
-=======
-  if (is.null(gene_sel) || is.null(gene_sel) || elpimode=="computeElasticPrincipalCircle" || nrow(gene_sel)<1) {
->>>>>>> aaf1b87bceb4aa23f6a58b71932ae5cd473f7891
->>>>>>> f9fa6a8fcbb037d6a3a46412d7e2633b5597133a
     return(NULL)
   }
   if (.schnappsEnv$DEBUGSAVE) {
@@ -721,15 +713,7 @@ traj_elpi_modules <- reactive({
   }
   # load(file="~/SCHNAPPsDebug/traj_elpi_modules.RData")
   
-<<<<<<< HEAD
   gene_sel = gene_sel$gene_sel
-=======
-<<<<<<< HEAD
-  gene_sel = gene_sel$gene_sel
-  
-=======
->>>>>>> aaf1b87bceb4aa23f6a58b71932ae5cd473f7891
->>>>>>> f9fa6a8fcbb037d6a3a46412d7e2633b5597133a
   set.seed(seed)
   expr_sel <- t(as.matrix(assays(scEx_log)[[1]][gene_sel$gene,]))
   
@@ -786,7 +770,6 @@ traj_elpi_gimp <- reactive({
   
   return(list(expr_sel = expr_sel, gene_sel = gene_sel))
 })
-<<<<<<< HEAD
 # 
 # # observeProj ----
 # # update projections
@@ -843,67 +826,6 @@ traj_elpi_gimp <- reactive({
 #     sessionProjections$prjs = prjs
 #   }
 # })
-=======
-
-<<<<<<< HEAD
-=======
-# observeProj ----
-# update projections
-observe({
-  start.time <- base::Sys.time()
-  on.exit({
-    printTimeEnd(start.time, "observeProj")
-    if (!is.null(getDefaultReactiveDomain()))
-      removeNotification(id = "observeProj")
-  })
-  if (!is.null(getDefaultReactiveDomain())) {
-    showNotification("observeProj", id = "observeProj", duration = NULL)
-  }
-  if (DEBUG) cat(file = stderr(), "observeProj started.\n")
-  
-  startNode <- input$elpiStartNode
-  endNode <- input$elpiEndNode
-  elpimode <- input$ElpiMethod
-  psTime = traj_getPseudotime()
-  scEx_log <- scEx_log()
-  isolate({
-    prjs <- sessionProjections$prjs
-  })
-  
-  if (is.null(scEx_log) || is.null(psTime) || elpimode=="computeElasticPrincipalCircle") {
-    return(NULL)
-  }
-  if (.schnappsEnv$DEBUGSAVE) {
-    save(file = "~/SCHNAPPsDebug/observeProj.RData", list = c(ls(), ls(envir = globalenv())))
-  }
-  # load(file="~/SCHNAPPsDebug/observeProj.RData")
-  cn = paste0("traj_", startNode, "_", endNode)
-  if (cn %in% colnames(prjs)) {
-    return(NULL)
-  }
-  # browser()
-  if (ncol(prjs) > 0) {
-    # make sure we are working with the correct cells. This might change when cells were removed.
-    prjs = prjs[colnames(scEx_log),,drop=FALSE]
-    # didn't find a way to easily overwrite columns
-    
-    if (cn %in% colnames(prjs)) {
-      prjs[, cn] <- psTime$Pt
-    } else {
-      prjs <- base::cbind(prjs, psTime$Pt, deparse.level = 0)
-      colnames(prjs)[ncol(prjs)] <- cn
-    }
-    sessionProjections$prjs <- prjs
-  } else {
-
-        prjs <- data.frame(cn = psTime$Pt )
-    rownames(prjs) = colnames(scEx_log)
-    colnames(prjs)[ncol(prjs)] = cn
-    sessionProjections$prjs = prjs
-  }
-})
->>>>>>> aaf1b87bceb4aa23f6a58b71932ae5cd473f7891
->>>>>>> f9fa6a8fcbb037d6a3a46412d7e2633b5597133a
 
 #traj_tragetPath ----
 traj_tragetPath <- reactive({
